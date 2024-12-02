61f75f50c2ebbdc641a409175eacffae1a20d3c1
Peter Heller <52469803+Frozenbitz@users.noreply.github.com>
2024-12-02 23:57:37 +0100

    very dirty fix for day 2

--- AoC2024-Day2/Program.cs
+++ AoC2024-Day2/Program.cs
@@ -28,10 +28,35 @@ class Program
                  
                 // we need to cast from strings to int, for processing
                 List<int> intList = splitLine.Select(int.Parse).ToList();
-                
-                if (CheckLineIsSafe(intList))
+
+                int tmpCheck = CheckLineIsSafe(intList);
+                if (tmpCheck == -1)
                 {
                     safeLines++;
+                }
+                else
+                {
+                    List<int> tmpList = intList;
+                    List<int> tmpList2 = intList;
+                    List<int> tmpList3 = intList;
+
+                    // remove first found issue and retry using the returned index and the next one
+                    tmpList.RemoveAt(tmpCheck);
+                    if (tmpCheck+1 < tmpList2.Count)
+                    {
+                        tmpList2.RemoveAt(tmpCheck + 1);
+                    }
+                    if (tmpCheck-1 < tmpList3.Count && tmpCheck-1 > 0)
+                    {
+                        tmpList3.RemoveAt(tmpCheck -1);
+                    }
+                    
+                    if ((CheckLineIsSafe(intList) == -1) ||
+                        (CheckLineIsSafe(tmpList2) == -1) || 
+                        CheckLineIsSafe(tmpList3) == -1)
+                    {
+                        safeLines++;
+                    }
                 }
             }
             
@@ -40,7 +65,7 @@ class Program
         }
     }
 
-    static bool CheckLineIsSafe(List<int> values)
+    static int CheckLineIsSafe(List<int> values)
     {
         // create adjacent pairs using array.zip 
         // creates touples? of values where we should be able to iterate over  the pairs and compare both
@@ -52,18 +77,24 @@ class Program
         {
             diffs.Add(value.current - value.next);
         }
+        
+        // get the initial sign
+        int initialSign = Math.Sign(diffs[0]);
 
         // check bounds
-        foreach (var difference in diffs)
+        foreach (var (difference, index) in diffs.Select((value, index) => (value, index)))
         {
-            if (Math.Abs(difference) > 3) return false;
-            if (Math.Abs(difference) == 0) return false;
+            // check if the signedness changes
+            if (Math.Sign(difference) != initialSign) return index;
+            if (Math.Abs(difference) > 3) return index;
+            if (Math.Abs(difference) == 0) return index;
         }
 
-        if (diffs.All(n => n > 0)) return true;
-        if (diffs.All(n => n < 0)) return true;
+        if (diffs.All(n => n > 0)) return -1;
+        if (diffs.All(n => n < 0)) return -1;
 
-        return false;
+        // is 0 a good value for fucked input?
+        return 0;
     }
 }
 
