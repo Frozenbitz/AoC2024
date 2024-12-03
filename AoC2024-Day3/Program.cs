ef87539b2134f4b0d4fe373fd179761eebe594b6
Peter Heller <52469803+Frozenbitz@users.noreply.github.com>
2024-12-03 20:42:08 +0100

    fixed the challenge p2

--- AoC2024-Day3/Program.cs
+++ AoC2024-Day3/Program.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using System.IO;
+using System.Text.RegularExpressions;
+
 
 class Program
 {
     static void Main()
     {
         
-        Console.WriteLine("AOC 2024 Day 2");
+        Console.WriteLine("AOC 2024 Day 3");
 
-        string filePath ="C:\\Users\\phell\\RiderProjects\\AoC2024\\AoC2024-Day2\\Inputs\\riddle.rmf";
+        string filePath ="C:\\Users\\phell\\RiderProjects\\AoC2024\\AoC2024-Day3\\Inputs\\riddle.rmf";
         if (! File.Exists(filePath))
         {
             Console.WriteLine("The file could not be found");
@@ -19,82 +21,39 @@ class Program
         using (StreamReader reader = new StreamReader(filePath))
         {
             string? line;
-            int safeLines = 0;
-
+            string pattern = @"(mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\))"; // a valid something looks like this: mul(a,b)
+            int multiValue = 0;
+            bool mulsEnabled = true;
+            
             while ((line = reader.ReadLine()) != null)
             {
-                // opening the provided buffer for testing 
-                string[] splitLine = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-                 
-                // we need to cast from strings to int, for processing
-                List<int> intList = splitLine.Select(int.Parse).ToList();
 
-                int tmpCheck = CheckLineIsSafe(intList);
-                if (tmpCheck == -1)
+                var matches = Regex.Matches(line, pattern);
+                foreach (Match match in matches)
                 {
-                    safeLines++;
-                }
-                else
-                {
-                    List<int> tmpList = intList;
-                    List<int> tmpList2 = intList;
-                    List<int> tmpList3 = intList;
-
-                    // remove first found issue and retry using the returned index and the next one
-                    tmpList.RemoveAt(tmpCheck);
-                    if (tmpCheck+1 < tmpList2.Count)
+                    if (match.Value == "do()")
                     {
-                        tmpList2.RemoveAt(tmpCheck + 1);
-                    }
-                    if (tmpCheck-1 < tmpList3.Count && tmpCheck-1 > 0)
-                    {
-                        tmpList3.RemoveAt(tmpCheck -1);
+                        mulsEnabled = true;
+                        continue;
                     }
                     
-                    if ((CheckLineIsSafe(intList) == -1) ||
-                        (CheckLineIsSafe(tmpList2) == -1) || 
-                        CheckLineIsSafe(tmpList3) == -1)
+                    if (match.Value == "don't()")
                     {
-                        safeLines++;
+                        mulsEnabled = false;
+                        continue;
+                    }
+
+                    if (mulsEnabled)
+                    {
+                        GroupCollection groups = match.Groups;
+                        multiValue += int.Parse(groups[2].Value) * int.Parse(groups[3].Value); 
                     }
                 }
             }
             
-            Console.WriteLine($"The number of safe lines is {safeLines}");
+            Console.WriteLine($"Uncorrupted Muls:  {multiValue}");
             
         }
-    }
-
-    static int CheckLineIsSafe(List<int> values)
-    {
-        // create adjacent pairs using array.zip 
-        // creates touples? of values where we should be able to iterate over  the pairs and compare both
-        var adjacentPairs = values.Zip(values.Skip(1), (current, next) => (current, next));
-        
-        // collect the diffs 
-        List<int> diffs = new List<int>();
-        foreach (var value in adjacentPairs)
-        {
-            diffs.Add(value.current - value.next);
-        }
-        
-        // get the initial sign
-        int initialSign = Math.Sign(diffs[0]);
-
-        // check bounds
-        foreach (var (difference, index) in diffs.Select((value, index) => (value, index)))
-        {
-            // check if the signedness changes
-            if (Math.Sign(difference) != initialSign) return index;
-            if (Math.Abs(difference) > 3) return index;
-            if (Math.Abs(difference) == 0) return index;
-        }
-
-        if (diffs.All(n => n > 0)) return -1;
-        if (diffs.All(n => n < 0)) return -1;
-
-        // is 0 a good value for fucked input?
-        return 0;
     }
 }
 
